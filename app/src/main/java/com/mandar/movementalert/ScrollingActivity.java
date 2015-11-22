1a12776f207850099f4af9daa0e321dca3f60269
Mandar <01.mandar@gmail.com>
2015-11-22 09:58:58 +0530

    published

--- app/src/main/java/com/mandar/movementalert/ScrollingActivity.java
+++ app/src/main/java/com/mandar/movementalert/ScrollingActivity.java
@@ -1,12 +1,12 @@
 package com.mandar.movementalert;
 
+
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -14,111 +14,169 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageView;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
+
+import java.util.Set;
 
 
 public class ScrollingActivity extends AppCompatActivity implements SensorEventListener {
 
     InterstitialAd mInterstitialAd;
+    AdView adView;
 
     RippleBackgroundV2 rippleBackground;
-    ImageView imageView;
-    MediaPlayer mediaPlayer;
+    ImageView radarimage;
+
     private SensorManager sensorManager;
     Vibrator v;
     long[] pattern = {0, 10, 100};
 
-    Uri notification;
-    Ringtone r;
-    private boolean update = true;
-    private boolean isStart = true;
-    boolean firstTimeReset = true;
+
+    private boolean update;
+    private boolean isStart;
     private float sxLow, syLow, szLow, sxHi, syHi, szHi, watermark;
+
+    Uri notification;
+
+    Ringtone r;
+
+    // UI
+    AlertDialog.Builder alert;
+    WebView wv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scrolling);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        rippleBackground=(RippleBackgroundV2)findViewById(R.id.content);
-        imageView=(ImageView)findViewById(R.id.centerImage);
-        rippleBackground.startRippleAnimation();
-
-        imageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final Uri currentTone = RingtoneManager.getActualDefaultRingtoneUri(getApplicationContext(), RingtoneManager.TYPE_ALARM);
-                Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
-                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALL);
-                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone");
-                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, currentTone);
-                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false);
-                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, true);
-                startActivityForResult(intent, 999);
-                if (mInterstitialAd.isLoaded()) {
-                    mInterstitialAd.show();
-                }
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "tune", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-
-
-
-        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-
-       // mediaPlayer = MediaPlayer.create(this, R.raw.sound1);
-
+        //
+        // Ringer
+        //
         v = (Vibrator) this.getSystemService(VIBRATOR_SERVICE);
-
-        notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
         r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-        watermark=1.52f;
 
         //
         // AdMob
         //
         mInterstitialAd = new InterstitialAd(this);
-        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
-
+        mInterstitialAd.setAdUnitId("ca-app-pub-2991750498895413/1445363761");
         mInterstitialAd.setAdListener(new AdListener() {
             @Override
             public void onAdClosed() {
                 requestNewInterstitial();
+            }
+        });
+        requestNewInterstitial();
+
+        AdView adView = (AdView)findViewById(R.id.adView);
+        AdRequest request = new AdRequest.Builder().build();
+        adView.loadAd(request);
+        //
+        // UI
+        //
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        rippleBackground = (RippleBackgroundV2) findViewById(R.id.content);
+        radarimage = (ImageView) findViewById(R.id.centerImage);
+       // rippleBackground.startRippleAnimation();
+        radarimage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isStart == true) {
+                    // now stop
+                    isStart=false;
+                    update=false;
+                    radarimage.setImageResource(R.drawable.on);
+                } else {
+                    // now stop
+                    isStart=true;
+                    update=true;
+                    radarimage.setImageResource(R.drawable.off);
+                }
+            }
+        });
+
+        // Alert box
+        //
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                try {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://play.google.com/store/apps/developer?id=Mandar+Shinde")));
+                } catch (android.content.ActivityNotFoundException anfe) {
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/developer?id=Mandar+Shinde")));
+                }
 
             }
         });
 
-        requestNewInterstitial();
+//
+//        FloatingActionButton fabringtone = (FloatingActionButton) findViewById(R.id.fabring);
+//        fabringtone.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                currentTone = RingtoneManager.getActualDefaultRingtoneUri(getApplicationContext(), RingtoneManager.TYPE_ALARM);
+//                Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+//                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TYPE, RingtoneManager.TYPE_ALL);
+//                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_TITLE, "Select Tone");
+//                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_EXISTING_URI, currentTone);
+//                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_SILENT, false);
+//                intent.putExtra(RingtoneManager.EXTRA_RINGTONE_SHOW_DEFAULT, true);
+//                startActivityForResult(intent, 999);
+//                if (mInterstitialAd.isLoaded()) {
+//                    mInterstitialAd.show();
+//                }
+//
+//            }
+//        });
 
+
+        //
+        // Sensor
+        //
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), 100000);
+
+        //
+        // Default Setting
+        //
+        watermark = 1.22f;
+        update=false;
+        isStart=false;
+        radarimage.setImageResource(R.drawable.on);
 
     }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        // Check which request we're responding to
+//        if (requestCode == 999) {
+//            // Make sure the request was successful
+//            if (resultCode == RESULT_OK) {
+//            }
+//        }
+//    }
 
     private void requestNewInterstitial() {
-        AdRequest adRequest = new AdRequest.Builder()
-                .addTestDevice("F71F7F0AADA8E16DFFEC2F4BA6638276")
-                .build();
-
+        AdRequest adRequest = new AdRequest.Builder().build();
         mInterstitialAd.loadAd(adRequest);
     }
 
@@ -132,13 +190,37 @@ public class ScrollingActivity extends AppCompatActivity implements SensorEventL
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            final String appPackageName = getPackageName(); // getPackageName() from Context or Activity object
+
+            alert = new AlertDialog.Builder(this);
+            wv = new WebView(this);
+
+            wv.loadUrl("file:///android_asset/index.html");
+            wv.setWebViewClient(new WebViewClient() {
+                @Override
+                public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                    view.loadUrl(url);
+                    return true;
+                }
+            });
+
+            alert.setView(wv);
+            alert.setNegativeButton("done", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int id) {
+                    dialog.dismiss();
+                }
+            });
+            alert.show();
+            if (mInterstitialAd.isLoaded()) {
+                mInterstitialAd.show();
+            }
+
+
             return true;
         }
         return super.onOptionsItemSelected(item);
@@ -147,68 +229,61 @@ public class ScrollingActivity extends AppCompatActivity implements SensorEventL
     ///
     /// Sensor Code
     ///
-
     @Override
     public void onSensorChanged(SensorEvent event) {
-        if(isStart==true)
-        {	if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
-        {
-            float[] values = event.values;
-            movement(values[0], values[1], values[2], (float) watermark);
-        }
+        if (isStart == true) {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                float[] values = event.values;
+                movement(values[0], values[1], values[2], (float) watermark);
+            }
         }
     }
 
-    public boolean movement(float a, float b, float c, double d)
-    {
-
-        if (update == true)
-        {
+    public boolean movement(float a, float b, float c, double d) {
+        if (update == true) {
             sxLow = (float) (a - d);
             syLow = (float) (b - d);
             szLow = (float) (c - d);
             sxHi = (float) (a + d);
             syHi = (float) (b + d);
             szHi = (float) (c + d);
-
             update = false;
         }
 
-        if ((sxLow > a) || (a > sxHi) || (syLow > b) || (b > syHi) || (szLow > c)  || (c > szHi))
-        {
-            try {
-            if(!r.isPlaying())
-            {
-                r.play();
-              //  v.vibrate(pattern, 0);
-                rippleBackground.startRippleAnimation();
-                imageView.setImageResource(R.drawable.nuke);
-            }
 
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        else
-        {
-            try {
-                if(r.isPlaying())
-                {
-                    r.stop();
-                    //v.cancel();
-
-
-                }
-                rippleBackground.stopRippleAnimation();
-                imageView.setImageResource(R.drawable.peace);
-                //
-
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+        if(((sxLow > a) || (a > sxHi) || (syLow > b) || (b > syHi) || (szLow > c) || (c > szHi))) {
+            SetAlarm();
+        } else {
+            UnSetAlarm();
         }
         return true;
+    }
+
+    void SetAlarm()
+    {
+        try {
+            if (!r.isPlaying())
+                r.play();
+
+            //  v.vibrate(pattern, 0);
+           // if(!rippleBackground.isRippleAnimationRunning())
+            rippleBackground.startRippleAnimation();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void UnSetAlarm()
+    {
+        try {
+            if (r.isPlaying())
+                r.stop();
+                rippleBackground.stopRippleAnimation();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
